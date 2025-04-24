66975fec716ce6c535e5318bbd8c09c0ea8f8ab6
Matthis Ebel <matthis.ebel@uni-greifswald.de>
2025-04-24 22:10:51 +0200

    Use loss with all current updates/changes

--- modules/model.py
+++ modules/model.py
@@ -357,13 +357,12 @@ class SpecificProfile(tf.keras.Model): # type: ignore
         Z = tf.math.multiply(Z, Zsm)
         # logging.debug(f"[model.lossfun] >>> \nZ: {Z}")
 
-        # Z = tf.maximum(Z, 0) # >>> TEST TEST TEST <<<
+        Z = tf.maximum(Z, 0)
         # logging.debug(f"[model.lossfun] >>> \nZ (2): {Z}")
         
         loss_by_unit = -tf.math.reduce_max(Z, axis=-1) # best isolated match for each profile in each genome (N x U)
         # logging.debug(f"[model.lossfun] >>> \nloss_by_unit: {loss_by_unit}\n  mean: {tf.reduce_mean(loss_by_unit)}, max: {tf.reduce_max(loss_by_unit)}")
         
-        # >>> TEST TEST TEST <<<
         def mellowmax(x, a):
             """ See https://en.wikipedia.org/wiki/Smooth_maximum#Mellowmax
               mm(x, a) = 1/a log( sum(exp(x*a)) / n ) --> ( log(sum(exp(x*a))) - log(n) ) / a 
@@ -382,8 +381,8 @@ class SpecificProfile(tf.keras.Model): # type: ignore
         # logging.debug(f"[model.lossfun] >>> \nmellowmax(loss,  1  ): {mellowmax(loss_by_unit, 1)}")
         # logging.debug(f"[model.lossfun] >>> \nmellowmax(loss, 10  ): {mellowmax(loss_by_unit, 10)}")
 
-        # loss_by_unit = mellowmax(loss_by_unit, self.setup.mellowmax_alpha)
-        loss_by_unit = tf.math.reduce_sum(loss_by_unit, axis=0) # best isolated match of all genomes (U,)
+        loss_by_unit = mellowmax(loss_by_unit, self.setup.mellowmax_alpha)
+        # loss_by_unit = tf.math.reduce_sum(loss_by_unit, axis=0) # best isolated match of all genomes (U,)
         # logging.debug(f"[model.lossfun] >>> \nloss_by_unit (2): {loss_by_unit}")
             
         if self.setup.l2 != 0:

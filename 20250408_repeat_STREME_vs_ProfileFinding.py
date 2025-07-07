15c81ab551d42e4d249fa0c90e786363fb33f97e
Matthis Ebel <matthis.ebel@uni-greifswald.de>
2025-07-07 15:10:31 +0200

    load python venv in slurm script

--- 20250408_repeat_STREME_vs_ProfileFinding.py
+++ 20250408_repeat_STREME_vs_ProfileFinding.py
@@ -95,6 +95,7 @@ refmotifs=({ref_motifs['ref'].str.cat(sep=' ')})
 refmotif=${{refmotifs[$SLURM_ARRAY_TASK_ID]}}
 
 source ~/Software/load_MEME.sh
+source ~/genomegraph/.venv/bin/activate
 
 """
     
